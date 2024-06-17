9fb234b3fe0b6b701be0abbd3259af795b01abd2
BleinerMathias <bleinerbbm@hotmail.com>
2024-06-17 08:56:10 -0300

    feat(convert): implement service to convert currency

--- app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/currencyconverter/model/api/CurrencyConverterApiService.kt
@@ -11,20 +11,20 @@ interface CurrencyConverterApiService {
 
     @Headers(
         "x-rapidapi-host: currency-converter5.p.rapidapi.com",
-        "x-rapidapi-key: "
+        "x-rapidapi-key: c41b657a7fmsh2e68703716b0594p1ac981jsn96a08f2cc0be"
     )
     @GET("list")
     fun getCurrencyList(): Call<CurrencyList> // Realiza a chamada para obter a lista de moedas (assincrona)
 
     @Headers(
         "x-rapidapi-host: currency-converter5.p.rapidapi.com",
-        "x-rapidapi-key:"
+        "x-rapidapi-key: c41b657a7fmsh2e68703716b0594p1ac981jsn96a08f2cc0be"
     )
     @GET("convert")
     fun convertCurrency(
         @Query("from") from: String,
         @Query("to") to: String,
-        @Query("amount") amount: Double
+        @Query("amount") amount: String
     ): Call<ConversionResult>
 
 
